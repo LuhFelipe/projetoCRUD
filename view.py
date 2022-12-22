bd4896c6522dd7c5f7dc37f867728fd6f134629c
LuhFelipe <lucasfelipegomespedrosa@gmail.com>
2022-12-22 19:02:41 -0300

    third commit

--- view.py
+++ view.py
@@ -12,13 +12,13 @@ con = lite.connect("dados.db")
 #D DELETE
 
 lista = ['Joao Futi Muanda','joao@mail.com', 123456789, "12/19/2010", 'Normal', 'gostaria de o consultar pessoalmente']
-"""
+
 #inserir Informações
 with con:
     cur = con.cursor()
     query = "INSERT INTO formulario (nome, email, telefone, diaEm, estado, assunto) VALUES (?, ?, ?, ?, ?, ?)"
     cur.execute(query, lista)
-"""
+
 #acessar Informações
 with con:
     cur = con.cursor()
@@ -27,19 +27,17 @@ with con:
     info = cur.fetchall()
     print(info)
 
-"""
+
 lista = ["joao", 1]
 #atualizar Informações
 with con:
     cur = con.cursor()
     query = "UPDATE formulario SET nome = ? WHERE id = ?"
     cur.execute(query, lista)
-"""
-"""
+
 lista = [1]
 #deletar Informações
 with con:
     cur = con.cursor()
     query = "DELETE FROM formulario WHERE id = ?"
     cur.execute(query, lista)
-    """
